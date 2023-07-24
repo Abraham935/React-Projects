edec0b2efbd0c7b573259441bc76e7dc9244443e
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2023-07-24 15:05:06 -0600

    Finished Patient list design

---
+++ patient-appointment/src/components/Patient.jsx
@@ -0,0 +1,33 @@
+
+
+export const Patient = () => {
+  return (
+    
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 uppercase text-gray-700">
+          Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 uppercase text-gray-700">
+          Propietary: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 uppercase text-gray-700">
+          Email: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 uppercase text-gray-700">
+          Date out: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 uppercase text-gray-700">
+          Synthomps: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+      </div>
+  )
+}
--- patient-appointment/src/components/PatientsList.jsx
+++ patient-appointment/src/components/PatientsList.jsx
@@ -1,7 +1,24 @@
+import { Patient } from "./Patient"
+
 function PatientsList() {
   return (
-    <div className="w-1/2  lg:w-3/5">
-      <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2  lg:w-3/5 ">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className=" text-xl mt-5 mb-10 text-center">
+        Manage your {''}
+        <span className="text-indigo-600 font-bold">Patients and Dates</span>
+
+      </p>
+
+      <div className=" md:h-screen overflow-y-scroll">
+        <Patient />
+        <Patient />
+        <Patient />
+        <Patient />
+        <Patient />
+      </div>
+
+      
 
     </div>
   )
