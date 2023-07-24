680665cc4dc3a32099b81357a5425a3bdaef83c7
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2023-07-24 14:48:54 -0600

    finished patients form

--- patient-appointment/src/components/Formulario.jsx
+++ patient-appointment/src/components/Formulario.jsx
@@ -7,8 +7,9 @@ function Formulario() {
             <span className="text-indigo-600 font-bold ">Manage them</span>
         </p>
 
-        <form className="bg-white shadow-sm rounded-lg py-10 px-5">
-          <div>
+        <form className="bg-white shadow-md rounded-lg py-10 px-5">
+
+          <div className="mb-5">
             <label htmlFor="mascota" className="block text-gray-700 uppercase">Pet´s name</label>
 
             <input
@@ -18,6 +19,55 @@ function Formulario() {
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             />
           </div>
+
+          <div className="mb-5">
+            <label htmlFor="mascota" className="block text-gray-700 uppercase">Person´s Name</label>
+
+            <input
+              id="mascota"
+              type="text"
+              placeholder="Person´s name"
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="mascota" className="block text-gray-700 uppercase">Email</label>
+
+            <input
+              id="email"
+              type="email"
+              placeholder="Email"
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="mascota" className="block text-gray-700 uppercase">Date of out</label>
+
+            <input
+              id="date"
+              type="date"
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="mascota" className="block text-gray-700 uppercase">Pet´s name</label>
+            <textarea
+              id="synthoms"
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              placeholder="Describe synthoms"
+            />
+          </div>
+
+          <input 
+            type="submit"
+            className="uppercase bg-indigo-600 w-full p-3 text-white font-bold hover:bg-indigo-700 transition-all"
+            value="Add pacient"
+          />
+
+
         </form>
     </div>
   )
