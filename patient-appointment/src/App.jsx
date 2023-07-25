b15c7db47110f041a5e2188c4cabbba63ea9c749
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2023-07-24 21:30:50 -0600

    Added Hooks to Form component and App to store the data from form component

--- patient-appointment/src/App.jsx
+++ patient-appointment/src/App.jsx
@@ -1,14 +1,21 @@
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import PatientsList from "./components/PatientsList"
+import {useState} from "react"
 
 function App() {
+  const [patients, setPatients] = useState([]);
 
+  console.log(patients)
+  
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario />
+        <Formulario
+          patients={patients} 
+          setPatients={setPatients}
+        />
         <PatientsList />
       </div>
     </div>
--- patient-appointment/src/components/Formulario.jsx
+++ patient-appointment/src/components/Formulario.jsx
@@ -1,4 +1,47 @@
-function Formulario() {
+import {useState, useEffect} from "react"
+
+function Formulario({patients, setPatients}) {
+  const [name, setName] = useState('');
+  const [owner, setOwner] = useState('');
+  const [email, setEmail] = useState('');
+  const [date, setDate] = useState('');
+  const [symptoms, setSymptoms] = useState('');
+  
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validate form
+    if([ name, owner, email, date, symptoms ].includes('')){
+      console.log("One field is empty")
+      setError(true);
+    } else {
+      setError(false)
+
+      //Patient object
+
+      const patientObject = {
+        name, 
+        owner, 
+        email, 
+        date, 
+        symptoms
+      }
+
+
+      setPatients([...patients, patientObject])
+
+      //Reset form
+
+      setName('');
+      setOwner('');
+      setEmail('');
+      setDate('');
+      setSymptoms('');
+    }
+  }
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
         <h2 className="font-black text-3xl text-center">Following Patients</h2>
@@ -7,57 +50,78 @@ function Formulario() {
             <span className="text-indigo-600 font-bold ">Manage them</span>
         </p>
 
-        <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <form 
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded-lg py-10 px-5">
+
+          
+          {error && 
+            <div className=" bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+              <p>All fields need to be fill</p>
+
+            </div>
+          
+          }
 
           <div className="mb-5">
-            <label htmlFor="mascota" className="block text-gray-700 uppercase">Pet´s name</label>
+            <label htmlFor="pet" className="block text-gray-700 uppercase">Pet´s name</label>
 
             <input
-              id="mascota"
+              id="pet"
               type="text"
               placeholder="Pet´s name"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={name}
+              onChange={ (e) => setName(e.target.value)}
             />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="mascota" className="block text-gray-700 uppercase">Person´s Name</label>
+            <label htmlFor="owner" className="block text-gray-700 uppercase">Person´s Name</label>
 
             <input
-              id="mascota"
+              id="owner"
               type="text"
-              placeholder="Person´s name"
+              placeholder="Owner´s name"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={owner}
+              onChange={ (e) => setOwner(e.target.value)}
             />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="mascota" className="block text-gray-700 uppercase">Email</label>
+            <label htmlFor="email" className="block text-gray-700 uppercase">Email</label>
 
             <input
               id="email"
               type="email"
               placeholder="Email"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={email}
+              onChange={ (e) => setEmail(e.target.value)}
             />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="mascota" className="block text-gray-700 uppercase">Date of out</label>
+            <label htmlFor="date" className="block text-gray-700 uppercase">Date of out</label>
 
             <input
               id="date"
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={date}
+              onChange={ (e) => setDate(e.target.value)}
             />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="mascota" className="block text-gray-700 uppercase">Pet´s name</label>
+            <label htmlFor="symptoms" className="block text-gray-700 uppercase">Pet´s name</label>
             <textarea
-              id="synthoms"
+              id="symptoms"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              placeholder="Describe synthoms"
+              placeholder="Describe symptoms"
+              value={symptoms}
+              onChange={ (e) => setSymptoms(e.target.value)}
             />
           </div>
 
