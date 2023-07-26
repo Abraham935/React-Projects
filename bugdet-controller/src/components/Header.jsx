6e33e06b79fb9cceae1a38888e473f499db90872
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2023-07-26 12:27:34 -0600

    Added message coomponent and validation when adding new budget

--- bugdet-controller/src/components/Header.jsx
+++ bugdet-controller/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import NewBudget from './NewBudget'
 
-const Header = () => {
-  return (
-    <header>
-        <h1>Expense Tracker</h1>
-        <NewBudget />
-    </header>
-  )
+const Header = ({budget, setBudget}) => {
+
+    return (
+        <header>
+            <h1>Expense Tracker</h1>
+            <NewBudget
+                budget={budget}
+                setBudget={setBudget}
+            />
+        </header>
+    )
 }
 
 export default Header
--- bugdet-controller/src/components/NewBudget.jsx
+++ bugdet-controller/src/components/NewBudget.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
+import { Children, useState } from 'react';
+import Message from './Message';
 
-const NewBudget = () => {
-  return (
-    <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
-            <div className='campo'>
-                <label>Budget </label>
-                <input
-                    className='nuevo-presupuesto'
-                    type='text'
-                    placeholder='Add your Budget' 
-                />
-            </div>
-            
-            <input type='submit' value='Add'/>
+const NewBudget = ({budget, setBudget}) => {
 
-        </form>
-    </div>
-  )
+    const [message, setMessage] = useState('');
+
+    const handleBudget = (e) => {
+        e.preventDefault();
+        if(!Number(budget) || Number(budget) < 0){
+            setMessage('Invalid Budget')
+        } else {
+            console.log('Is a number')
+        }
+
+    }
+    
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handleBudget} className='formulario'>
+                <div className='campo'>
+                    <label>Budget </label>
+                    <input
+                        className='nuevo-presupuesto'
+                        type='text'
+                        placeholder='Add your Budget'
+                        value={budget}
+                        onChange={(e) => setBudget(e.target.value)} 
+                    />
+                </div>
+                
+                <input type='submit' value='Add'/>
+
+                {message && <Message type={"error"}>{message}</Message>}
+
+            </form>
+        </div>
+    )
 }
 
 export default NewBudget
