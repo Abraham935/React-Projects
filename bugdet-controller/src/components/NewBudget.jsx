b6e718346286545a23d3ffa74c03dd5a809ff775
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2023-07-26 12:06:56 -0600

    Header and Budget component added along with CSS updated

---
+++ bugdet-controller/src/components/NewBudget.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NewBudget = () => {
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form className='formulario'>
+            <div className='campo'>
+                <label>Budget </label>
+                <input
+                    className='nuevo-presupuesto'
+                    type='text'
+                    placeholder='Add your Budget' 
+                />
+            </div>
+            
+            <input type='submit' value='Add'/>
+
+        </form>
+    </div>
+  )
+}
+
+export default NewBudget
