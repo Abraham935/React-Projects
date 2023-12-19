74c9fced46c3a661d4beb1c276930d40adf87884
Abraham-Mendoza <Abraham_935@hotmail.com>
2023-12-18 22:55:52 -0600

    Multiple returns

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -101,6 +101,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt="Pizza" />
