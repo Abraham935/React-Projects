cf09c1d2d54c34517dedf0b8a6cc72b8b73d01e8
Abraham-Mendoza <Abraham_935@hotmail.com>
2023-12-20 19:22:35 -0600

    Added destructuring

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -100,16 +100,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt="Pizza" />
+      <img src={pizzaObj.photoName} alt="Pizza" />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price + 3}</span>
       </div>
     </li>
   );
@@ -131,10 +131,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We´re open until {closeHour}:00. Come Visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order openHour={openHour} closeHour={closeHour} />
       ) : (
         <p>
           We´re happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
@@ -142,6 +139,18 @@ function Footer() {
       )}
     </footer>
   );
+
+  function Order({ openHour, closeHour }) {
+    return (
+      <div className="order">
+        <p>
+          We´re open from {openHour}:00 to {closeHour}:00. Come Visit us or
+          order online
+        </p>
+        <button className="btn">Order</button>
+      </div>
+    );
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
